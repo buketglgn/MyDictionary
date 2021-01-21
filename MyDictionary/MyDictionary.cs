4ab9f4d949f91d6362fceec59bd60dc61a324424
Lenovo <Lenovo@LAPTOP-1BMVK9VI>
2021-01-21 22:24:28 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<Tkey, Tvalue>
+    {
+        Tkey[] keys;
+        Tvalue[] values;
+
+        public MyDictionary()
+        {
+            keys = new Tkey[0];
+            values = new Tvalue[0];
+        }
+
+        public void Add(Tkey keyItem, Tvalue valueItem)
+        {
+            Tkey[] tempKeys = keys;
+            Tvalue[] tempValues = values;
+
+            keys = new Tkey[keys.Length + 1];
+            values = new Tvalue[values.Length + 1];
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+                values[i] = tempValues[i];
+            }
+
+            keys[keys.Length - 1] = keyItem;
+            values[values.Length - 1] = valueItem;
+
+        }
+
+        public void ListValueByKey(Tkey keyItem)
+        {
+            for (int i = 0; i <keys.Length; i++)
+            {
+                if (keys[i].Equals(keyItem))
+                {
+                    Console.WriteLine("bulundu: "+ keys[i]+ " "+values[i]);
+                }
+               
+            }
+        }
+
+        public void ListAllValue()
+        {
+            for (int i = 0; i < keys.Length; i++)
+            {
+                Console.WriteLine(keys[i] + " " + values[i]);
+            }
+            
+
+        }
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            MyDictionary<int, string> myDictionary = new MyDictionary.MyDictionary<int, string>();
+            myDictionary.Add(10, "Buket");
+            myDictionary.Add(15, "Ceylin");
+            myDictionary.Add(20, "Bertan");
+            myDictionary.Add(25, "Gülsevim");
+            myDictionary.Add(30, "Bülent");
+
+            myDictionary.ListAllValue();
+            Console.WriteLine("********");
+            myDictionary.ListValueByKey(20);
+            myDictionary.ListValueByKey(10);
+        }
+    }
+}
